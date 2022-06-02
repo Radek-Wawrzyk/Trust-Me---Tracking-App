3788ca5bc9e8274212c6eb2220d76c44b041b71a
Radziu <radekwawrzyk@gmail.com>
2022-06-02 09:12:55 +0200

    Setup - Yarn workspace for managing both applications

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,3 +2,21 @@
   <h1>app</h1>
 </template>
 
+<script lang="ts">
+import { defineComponent, onMounted } from "vue";
+
+export default defineComponent({
+  setup() {
+    onMounted(async () => {
+      try {
+        const testRequest = await fetch('http://localhost:3000/');
+        const data = await testRequest.json();
+
+        console.log('Success', data);
+      } catch (error) {
+        console.log('Err', error);
+      }
+    })
+  },
+});
+</script>
